29c0a7c2eb7ebf97e53ab2a8daaf12be17917840
intelfan828 <andrewcolins828@gmail.com>
2025-06-05 14:45:25 -0400

    show svg using container

--- src/components/SVGViewer.tsx
+++ src/components/SVGViewer.tsx
@@ -7,17 +7,35 @@ interface SVGViewerProps {
 const SVGViewer: React.FC<SVGViewerProps> = ({ svgContent }) => {
   return (
     <div style={{ 
-      width: '100%', 
-      height: '100%',
+      width: '100vw', 
+      height: '100vh',
       display: 'flex',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+      backgroundColor: 'gray',
+      margin: 0,
+      padding: 0,
+      overflow: 'hidden'
     }}>
-      {svgContent && (
-        <div 
-          dangerouslySetInnerHTML={{ __html: svgContent }} 
-        />
-      )}
+      <div style={{
+        width: '95vw',
+        height: '95vh',
+        border: '1px solid #444444',
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        borderRadius: '10px'
+      }}>
+        {svgContent && (
+          <div 
+            style={{
+              width: '100%',
+              height: '100%',
+              overflow: 'auto'
+            }}
+            dangerouslySetInnerHTML={{ __html: svgContent }} 
+          />
+        )}
+      </div>
     </div>
   );
 };
