b45c757db6901213c7604be0e4fbd3de474100bb
intelfan828 <andrewcolins828@gmail.com>
2025-06-05 14:15:11 -0400

    update connectData

--- src/components/connectData.ts
+++ src/components/connectData.ts
@@ -1,16 +1,20 @@
 export const blockData = [
     {
-        id: 'block-id',   // id from svg file.
+        id: "g1778",
         lines: [
-            'g3884', 'g3880', 'g3888'
+            "g3884", "g3880", "g3888"
         ],
         data: {
-            name: 'block-name',
-            description: 'block-description',
+            name: "DC Engineering",
+            description: "DC Engineering",
             qa: [
                 {
-                    question: 'question-1',
-                    answer: 'answer-1',
+                    question: "What is the primary function of your team?",
+                    answer: "The DCS Infrastructure and Engineering team focuses on designs and standards to ensure a stable, resilient datacenter environment for JPMC. This includes 1.data center design and construction engagement, 2.physical infrastructure (cable plant) design and build, 3.product and platform development with Infrastructure Platforms hardware engineering teams, 4.design of “engineered solutions” that includes offsite integration of compute/storage/build infrastructure, and product management for DCS."
+                },
+                {
+                    question: "What is your team ultimately accountable for?",
+                    answer: "Our team's primary goal is to focus on designs and standards to ensure a stable, resilient datacenter environment for JPMC. This includes project/program/product support throughout DCS and ownership of all engineered solutions for product deployment at a global level."
                 }
             ]
         }
@@ -19,12 +23,12 @@ export const blockData = [
 
 export const connectData = [
     {
-        id: 'connect-id',
-        to: 'block-id',
-        from: 'block-id',
+        id: "connect-id",
+        to: "block-id",
+        from: "block-id",
         data: {
-            name: 'connect-name',
-            description: 'connect-description',
+            name: "connect-name",
+            description: "connect-description",
         }
     }
 ]
