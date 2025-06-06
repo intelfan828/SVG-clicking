7298cfe5311953f7c42b36e21394e850374b96a9
intelfan828 <andrewcolins828@gmail.com>
2025-06-06 08:10:09 -0400

    update connectData.ts

--- src/Data/connectData.ts
+++ src/Data/connectData.ts
@@ -1,12 +1,25 @@
 
 export const connectData = [
     {
-        id: "connect-id",
-        to: "block-id",
-        from: "block-id",
+        id: "g3884",
+        to: "g2378",
+        from: "g1778",
         data: {
-            name: "connect-name",
-            description: "connect-description",
+            name: "DC Engineering --> DC Exit & Strategy (DCE&S)",
+            summary: "Technical deployment & support",
+            desTitle: "DC Engineering —— DCE&S:",
+            description: "DC Engineering owns global standards and hardware design patterns; DCE&S applies these designs in site-specific exit or lifecycle scenarios. DCE&S consults Engineering for design feasibility and adaptations when standard patterns (e.g., Micro/Mini deployments) require tailoring due to location-specific constraints.",
         }
-    }
+    },
+    {
+        id: "g3884",
+        to: "g3888",
+        from: "g1778",
+        data: {
+            name: "DC Engineering --> Capacity & Placement Capacity",
+            summary: "Design and Standards",
+            desTitle: "DC Engineering —— Capacity:",
+            description: "Capacity collects business-side demand and routes it to Engineering (and sometimes TS or Ops. TS is optional in project support. ) Engineering provides the “what” — meaning engineered solution requirements for new tech.",
+        }
+    },
 ]
